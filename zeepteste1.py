1acadee3a3bfd8eadff3b80afb32860802805665
Gekson Cinaty <gekson.cinaty@indt.org.br>
2020-08-24 01:44:59 -0300

    test soap request

--- zeepteste1.py
+++ zeepteste1.py
@@ -1,8 +1,9 @@
 import zeep
+import json
 
 # wsdl = 'http://www.soapclient.com/xml/soapresponder.wsdl'
 # wsdl = 'http://www.byjg.com.br/site/webservice.php/ws/cep?WSDL'
-wsdl = 'http://INDTs-MacBook-Pro.local:8088/mockNumberConversionSoapBinding?wsdl'
+wsdl = 'http://INDTs-MacBook-Pro.local:8088/mockXacuteWSSoap?wsdl'
 
 client = zeep.Client(wsdl=wsdl)
 # print(client.service.obterCEP('Zeep TESTE', 'is cool', 'DF'))
@@ -13,4 +14,14 @@ request_data = {
     'UF': 'DF'
 }
 
-print(client.service.NumberToDollars(10))
+print(client.service.Xacute())
+
+print(client.service.Xacute()["Row"][0]["cd_posto"])
+
+# a = zeep.helpers.serialize_object(client.service.Xacute())
+
+# json_object_a = json.loads(json.dumps(a))
+
+# print(json_object_a)
+
+# print(json_object_a["Row"][0]["cd_posto"])
